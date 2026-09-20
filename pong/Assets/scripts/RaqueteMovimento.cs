6566a7fa67433203de351e777fb3d7fc27522b42
pietroguilerme <197247545+petroxsz@users.noreply.github.com>
2026-09-20 17:57:12 -0300

    salvando

--- pong/Assets/scripts/RaqueteMovimento.cs
+++ pong/Assets/scripts/RaqueteMovimento.cs
@@ -10,8 +10,27 @@ public class RaqueteMovimento : MonoBehaviour
     [Header("Jogador")]
     public bool jogador1 = true;
 
+    private TCPManager tcp;
+
+    void Start()
+    {
+        tcp = TCPManager.Instance;
+
+        tcp.AoReceberMensagem += ReceberMensagemRede;
+    }
+
     void Update()
     {
+        if (tcp == null || !tcp.conectado)
+            return;
+
+        bool souDonoDaRaquete =
+            (jogador1 && tcp.souHost) ||
+            (!jogador1 && !tcp.souHost);
+
+        if (!souDonoDaRaquete)
+            return;
+
         float movimento = 0f;
 
         if (jogador1)
@@ -44,5 +63,73 @@ public class RaqueteMovimento : MonoBehaviour
         );
 
         transform.position = posicao;
+
+        // Só envia enquanto estiver se movimentando
+        if (movimento != 0f)
+{
+    int numeroJogador = jogador1 ? 1 : 2;
+
+    string mensagem =
+        "RAQUETE:" +
+        numeroJogador + ":" +
+        transform.position.y.ToString(
+            System.Globalization.CultureInfo.InvariantCulture
+        );
+
+    Debug.Log("ENVIANDO: " + mensagem);
+
+    tcp.EnviarMensagem(mensagem);
+}
+    }
+
+    private void ReceberMensagemRede(string mensagem)
+    {
+        Debug.Log("RECEBI: " + mensagem);
+        
+        if (!mensagem.StartsWith("RAQUETE:"))
+            return;
+
+        string[] dados = mensagem.Split(':');
+
+        if (dados.Length != 3)
+            return;
+
+        int numeroJogador = int.Parse(dados[1]);
+
+        float posicaoY;
+
+        if (!float.TryParse(
+            dados[2],
+            System.Globalization.NumberStyles.Float,
+            System.Globalization.CultureInfo.InvariantCulture,
+            out posicaoY))
+        {
+            return;
+        }
+
+        if (jogador1 && numeroJogador == 1)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                posicaoY,
+                transform.position.z
+            );
+        }
+        else if (!jogador1 && numeroJogador == 2)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                posicaoY,
+                transform.position.z
+            );
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (tcp != null)
+        {
+            tcp.AoReceberMensagem -= ReceberMensagemRede;
+        }
     }
 }
